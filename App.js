a95d695218cdc2158800581ca27973ca9d421b3f
Youssef <ynakles@gmail.com>
2018-09-18 00:09:59 +0000

    test

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
       return (
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
+          <AppNavigator/>
         </View>
       );
     }
