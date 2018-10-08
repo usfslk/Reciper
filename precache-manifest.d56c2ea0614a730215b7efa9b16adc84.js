a6bef6a4351ec73c6422eed119ed9be559b64167
Youssef <ynakles@gmail.com>
2018-10-08 04:15:31 +0100

    Updates

---
+++ precache-manifest.d56c2ea0614a730215b7efa9b16adc84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0cebb483d4d01890e24",
+    "url": "/lumen/static/js/runtime~main.f0cebb48.js"
+  },
+  {
+    "revision": "6d17901ecc0778a89865",
+    "url": "/lumen/static/js/main.6d17901e.chunk.js"
+  },
+  {
+    "revision": "bde03651bc76b77dd24a",
+    "url": "/lumen/static/js/1.bde03651.chunk.js"
+  },
+  {
+    "revision": "6d17901ecc0778a89865",
+    "url": "/lumen/static/css/main.1e9daffe.chunk.css"
+  },
+  {
+    "revision": "bde03651bc76b77dd24a",
+    "url": "/lumen/static/css/1.7e1d1674.chunk.css"
+  },
+  {
+    "revision": "ee5668ff39bad98413a62a6b2c2b9bb1",
+    "url": "/lumen/index.html"
+  }
+];
